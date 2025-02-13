5251fa4dbeed865c8b7fbc5984c0d7e09a3760e6
Abdumutal01 <mamirovabdumutal5@gmail.com>
2025-02-13 20:59:02 +0500

    update

--- home/models.py
+++ home/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class Venue(models.Model):
-    venue_image = models.ImageField(null=True, blank=True, upload_to='media/ ')
+    venue_image = models.ImageField(null=True, blank=True, upload_to='venues/')
--- home/urls.py
+++ home/urls.py
@@ -7,8 +7,7 @@ from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home')
-] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] )
+] 
 
-1
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
